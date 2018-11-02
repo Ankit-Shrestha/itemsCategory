afb318f523fc9b66e4aa96dd3133eaa285616267
Ankit Shrestha <ankieet8030@gmail.com>
2018-11-01 22:46:36 -0400

    test

--- additems.py
+++ additems.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Categories, Base, Items, User
-engine = create_engine('postgresql://catalog.db:surviver123@localhost/catalog')
+engine = create_engine('postgresql://catalog.db:\'surviver123\'@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
